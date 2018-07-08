403a96973243811405dba0eb029e1abd27a3d4d8
brian31988 <brian31988@gmail.com>
2018-07-08 17:39:10 -0400

    set up form on create user page

--- src/Components/Pages/CreateAccountPage/CreateAccountPage.js
+++ src/Components/Pages/CreateAccountPage/CreateAccountPage.js
@@ -2,34 +2,46 @@ import React, { Component } from 'react';
 
 class CreateAccountPage extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-      }
-    
-      render() {
-        return (
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Name:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-        );
-      }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
 
-    export default CreateAccountPage;
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUsernameChange(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.username);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username:
+              <input type="text" value={this.state.username} onChange={this.handleUsernameChange} />
+        </label>
+        <label>
+          Password:
+              <input type="text" value={this.state.password} onChange={this.handlePasswordChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default CreateAccountPage;
